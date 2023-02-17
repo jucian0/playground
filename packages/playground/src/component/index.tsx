80625d3d1fb1e4f160be62bea5b4a9b2e8ed4a2f
Jucian0 <juciano@outlook.com.br>
2023-02-17 08:48:32 -0300

    adding style in playground component

--- packages/playground/src/component/index.tsx
+++ packages/playground/src/component/index.tsx
@@ -5,9 +5,17 @@ import { Resizable } from "re-resizable";
 import React from "react";
 import { getResizableProps } from "./resizableProps";
 import "../styles.css";
+import {
+  RxViewGrid,
+  RxCode,
+  RxCopy,
+  RxMobile,
+  RxDesktop,
+  RxTable,
+} from "react-icons/rx";
 
 export const Playground = (props: any) => {
-  const [editor, setEditor] = React.useState(false);
+  const [editor, setEditor] = React.useState(true);
   const [width, setWidth] = React.useState("100%");
 
   const resizableProps = getResizableProps(width, setWidth);
@@ -17,21 +25,54 @@ export const Playground = (props: any) => {
       {...resizableProps}
       className={` bg-white dark:bg-gray-800 dark:border-gray-700 border-gray-200 rounded-lg shadow border ${props.className}`}
     >
-      <div className=" bg-gray-200 rounded-t-lg flex justify-end p-2">
-        <button
-          type="button"
-          onClick={() => setEditor(!editor)}
-          className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-        >
-          Code
-        </button>
-        <button
-          type="button"
-          onClick={() => setEditor(!editor)}
-          className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-        >
-          Copy
-        </button>
+      <div className=" bg-gray-200 rounded-t-lg flex justify-between p-2">
+        <div></div>
+        <div>
+          <button
+            type="button"
+            onClick={() => setEditor(!editor)}
+            className="inline-block px-6 py-2.5 mr-2 bg-transparent text-black-600 font-medium text-xs leading-tight uppercase rounded hover:text-back-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out"
+          >
+            <RxMobile size={18} />
+          </button>
+          <button
+            type="button"
+            onClick={() => setEditor(!editor)}
+            className="inline-block px-6 py-2.5 mr-2 bg-transparent text-black-600 font-medium text-xs leading-tight uppercase rounded hover:text-back-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out"
+          >
+            <RxTable size={18} />
+          </button>
+          <button
+            type="button"
+            onClick={() => setEditor(!editor)}
+            className="inline-block px-6 py-2.5 bg-transparent text-back-600 font-medium text-xs leading-tight uppercase rounded hover:text-back-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out"
+          >
+            <RxDesktop size={18} />
+          </button>
+        </div>
+        <div>
+          <button
+            type="button"
+            onClick={() => setEditor(!editor)}
+            className="inline-block px-6 py-2.5 mr-2 bg-transparent text-black-600 font-medium text-xs leading-tight uppercase rounded hover:text-back-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out"
+          >
+            <RxViewGrid size={18} />
+          </button>
+          <button
+            type="button"
+            onClick={() => setEditor(!editor)}
+            className="inline-block px-6 py-2.5 mr-2 bg-transparent text-black-600 font-medium text-xs leading-tight uppercase rounded hover:text-back-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out"
+          >
+            <RxCode size={18} />
+          </button>
+          <button
+            type="button"
+            onClick={() => setEditor(!editor)}
+            className="inline-block px-6 py-2.5 bg-transparent text-back-600 font-medium text-xs leading-tight uppercase rounded hover:text-back-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out"
+          >
+            <RxCopy size={18} />
+          </button>
+        </div>
       </div>
 
       <LiveProvider
@@ -40,9 +81,11 @@ export const Playground = (props: any) => {
         theme={dark}
         frameBorder={2}
       >
-        <LivePreview className="p-2 overflow-auto h-auto rounded-b-lg" />
+        <div className="inset-0 bg-grid-slate-100 ">
+          <LivePreview className="p-2 overflow-auto h-auto rounded-b-lg" />
+        </div>
         {editor && (
-          <LiveEditor className="border-gray-200 border-t rounded-b-lg font-mono text-sm" />
+          <LiveEditor className="border-gray-200 border-t rounded-b-lg font-mono text-sm p-2" />
         )}
         <LiveError className="text-white bg-red-500 p-2 rounded-b-lg" />
       </LiveProvider>
--- packages/playground/tailwind.config.js
+++ packages/playground/tailwind.config.js
@@ -1,4 +1,9 @@
 const colors = require("tailwindcss/colors");
+const svgToDataUri = require("mini-svg-data-uri");
+
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 module.exports = {
   content: [`src/**/*.{js,tsx}`],
@@ -10,6 +15,26 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "bg-grid": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+
+      matchUtilities(
+        {
+          highlight: (value) => ({ boxShadow: `inset 0 1px 0 0 ${value}` }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
   darkMode: "class",
 };
