4dd96640f5ca9c778f4053775ea36a3deda63cc7
Jucian0 <juciano@outlook.com.br>
2023-02-16 21:11:21 -0300

    add tailwindcss

---
+++ docs/src/pages/_app.tsx
@@ -0,0 +1,4 @@
+import "@engrafia/playground/styles.css";
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ packages/playground/tailwind.config.js
@@ -0,0 +1,14 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [`src/**/*.{js,tsx}`],
+  theme: {
+    extend: {
+      colors: {
+        brandblue: colors.blue[500],
+        brandred: colors.red[500],
+      },
+    },
+  },
+  plugins: [],
+};
