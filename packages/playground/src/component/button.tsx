4b94a6c14ebe2c741ccac6674fad60115f666e3f
jucian0 <juciano@outlook.com.br>
2023-02-19 00:43:45 -0300

    fix isActive button feature

--- packages/playground/src/component/button.tsx
+++ packages/playground/src/component/button.tsx
@@ -10,8 +10,8 @@ export function Button({ children, className, isActive, ...props }: Props) {
       type="button"
       {...props}
       className={`${
-        isActive && "bg-gray-100"
-      } inline-block px-6 py-2.5 mr-2 bg-transparent text-black-600 font-medium text-xs leading-tight uppercase rounded hover:text-back-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out ${className}`}
+        isActive ? "bg-gray-100" : "bg-transparent"
+      } inline-block px-6 py-2.5 mr-2 text-black-600 font-medium text-xs leading-tight uppercase rounded hover:text-back-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out ${className}`}
     >
       {children}
     </button>
